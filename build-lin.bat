86f6b891f662a1926e51cebb2e61d932d8b3792a
vssl256 <adamantum22@gmail.com>
2025-10-07 04:28:13 +0300

    jar

--- build-lin.bat
+++ build-lin.bat
@@ -1,3 +1,4 @@
 @echo on
-rd /s /q jre GSim-lin
-jlink --module-path "jdk11\jmods" --add-modules java.base,javafx.controls,javafx.fxml --output jre --strip-debug --compress=2 --no-header-files --no-man-pages && java -jar packr-all-4.0.0.jar --platform linux64 --jdk jre --executable GSim --classpath gsim.jar --mainclass App --output GSim-lin
+rd /s /q GSim-lin
+::jlink --module-path "jdk11\jmods" --add-modules java.base,javafx.controls,javafx.fxml --output jre --strip-debug --compress=2 --no-header-files --no-man-pages && 
+java -jar packr-all-4.0.0.jar --platform linux64 --jdk jre --executable GSim --classpath gsim.jar --mainclass App --output GSim-lin
